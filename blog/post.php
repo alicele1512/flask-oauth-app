77988574520c3a32b06b43f8c31edb84f6f2cb1a
alicele1512 <45648174+alicele1512@users.noreply.github.com>
2024-12-23 16:20:31 +0700

    Create post.php

---
+++ blog/post.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once "../core/functions.php";
+
+if (!isUserLoggedIn()) {
+    header('Location: /login');
+    exit();
+}
+
+require_once "../pages/header.php";
+require_once "../pages/navbar.php";
+
+// Content for logged-in users
+echo "<h1>Blog Post Page</h1>";
+echo "<p>Welcome to the blog post page. Only logged-in users can see this content.</p>";
+
+require_once "../pages/footer.php";
+?>

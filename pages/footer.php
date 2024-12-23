3ad483b9da775ac344872f3d591b75c33549ac6e
alicele1512 <45648174+alicele1512@users.noreply.github.com>
2024-12-23 16:14:08 +0700

    Create footer.php

---
+++ pages/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <!-- Your footer content here -->
+</footer>
+<script async src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js'></script>
+<script async src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js'></script>
+<script async src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script defer src='assets/js/video.js'></script>
+<script defer src='blog.js'></script>
+</body>
+</html>
